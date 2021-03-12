bb888ab607abe9d6b119aa478bc653e0f90745ce
LeRoy Gardner <leroy.gardner@gmail.com>
2021-03-11 20:27:24 -0600

    add bootstrap cdn

--- password_gen/generator/views.py
+++ password_gen/generator/views.py
@@ -17,14 +17,12 @@ def password(request):
 
     if request.GET.get('uppercase'):
         characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
-    
     if request.GET.get('special'):
-        characters.extend(list('!@#$%^&*'))
-    
+        characters.extend(list('!@#$%^&*'))   
     if request.GET.get('numbers'):
         characters.extend(list('0123456789'))
-
     thepassword = ''
+    
     for x in range(length):
         thepassword += random.choice(characters)
 
