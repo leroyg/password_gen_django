c0ba2f944844d79efe89cb6a59653a196876be50
LeRoy Gardner <leroy.gardner@gmail.com>
2021-03-12 18:07:33 -0600

    blog added

---
+++ personal_portfolio_project/blog/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=250)
+    date = models.DateField()
+    description = models.TextField(max_length=2000)
