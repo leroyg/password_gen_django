4aaea6f0c1c7982cd32392a94461e49c017e0159
LeRoy Gardner <leroy.gardner@gmail.com>
2021-03-13 07:37:54 -0600

    fixed the admin for blog app

--- personal_portfolio_project/blog/models.py
+++ personal_portfolio_project/blog/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=250)
     date = models.DateField()
-    description = models.TextField(max_length=2000)
+    description = models.TextField(max_length=500)
--- personal_portfolio_project/portfolio/admin.py
+++ personal_portfolio_project/portfolio/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Project
-from .models import Blog
+
 
 admin.site.register(Project)
-admin.site.register(Blog)
